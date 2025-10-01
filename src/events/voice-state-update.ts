f63c41dee420cbec83c48f60965e708a121b356f
yorunoken <me@yorunoken.com>
2025-10-01 20:17:41 +0300

    dfgfd

--- src/events/voice-state-update.ts
+++ src/events/voice-state-update.ts
@@ -13,12 +13,12 @@ export default {
             const joinedUserId = newState.member?.id;
 
             if (joinedUserId === specificUserId) {
-                await webhook.send(`Specific user ${newState.member?.user.tag} joined any channel: ${newState.channel?.name}`);
+                await webhook.send(`\`\`\`Specific user ${newState.member?.user.tag} joined channel: "${newState.channel?.name}"\`\`\``);
             }
 
             const targetMember = newState.guild?.members.cache.get(targetUserId);
             if (targetMember?.voice.channelId === newState.channelId && joinedUserId !== targetUserId) {
-                await webhook.send(`${newState.member?.user.tag} joined the same VC as ${targetMember.user.tag}: ${newState.channel?.name}`);
+                await webhook.send(`\`\`\`${newState.member?.user.tag} joined the same VC as ${targetMember.user.tag}: "${newState.channel?.name}"\`\`\``);
             }
         }
 
@@ -26,12 +26,12 @@ export default {
             const leftUserId = oldState.member?.id;
 
             if (leftUserId === specificUserId) {
-                await webhook.send(`Specific user ${oldState.member?.user.tag} left channel: ${oldState.channel?.name}`);
+                await webhook.send(`\`\`\`Specific user ${oldState.member?.user.tag} left channel: "${oldState.channel?.name}"\`\`\``);
             }
 
             const targetMember = oldState.guild?.members.cache.get(targetUserId);
             if (targetMember?.voice.channelId === oldState.channelId && leftUserId !== targetUserId) {
-                await webhook.send(`${oldState.member?.user.tag} left the same VC as ${targetMember.user.tag}: ${oldState.channel?.name}`);
+                await webhook.send(`\`\`\`${oldState.member?.user.tag} left the same VC as ${targetMember.user.tag}: "${oldState.channel?.name}"\`\`\``);
             }
         }
     },
